4e791eb76c800f78ec8de260dd9c92a81b59cd02
harshal <harshalsonawane665@gmail.com>
2025-12-18 18:56:21 +0530

    done with task

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from core.views import dashboard
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("api/", include("core.urls")),
-#     path("dashboard/", dashboard, name="dashboard"),
-# ]
-
-
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
