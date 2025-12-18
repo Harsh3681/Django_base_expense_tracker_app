2b88b6b6bfe65f2d08e8265c530ffe3d0bd4e4f6
harshal <harshalsonawane665@gmail.com>
2025-12-18 12:07:22 +0530

    updated urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,23 @@
+# from django.contrib import admin
+# from django.urls import path, include
+# from core.views import dashboard
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("api/", include("core.urls")),
+#     path("dashboard/", dashboard, name="dashboard"),
+# ]
+
+
 from django.contrib import admin
 from django.urls import path, include
-from core.views import dashboard
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    return redirect("/dashboard/")
 
 urlpatterns = [
+    path("", root_redirect),
     path("admin/", admin.site.urls),
-    path("api/", include("core.urls")),
-    path("dashboard/", dashboard, name="dashboard"),
+    path("", include("core.urls")),
 ]
