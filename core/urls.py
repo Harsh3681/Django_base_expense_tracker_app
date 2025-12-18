8d9fad06d2fa8b2d3cd1866b2fe5effe36ff7cb7
harshal <harshalsonawane665@gmail.com>
2025-12-18 12:44:24 +0530

    Fix latest_rates API route

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,8 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     ExpenseViewSet,
     dashboard,
+    sync_exchange_rate,
+    latest_rates
 )
 
 router = DefaultRouter()
@@ -26,6 +28,10 @@ router.register("expenses", ExpenseViewSet, basename="expense")
 urlpatterns = [
     # API
     path("api/", include(router.urls)),
+
+    # integrations
+    path("integrations/exchange-rate/", sync_exchange_rate),
+    path("integrations/rates/", latest_rates),
 
     # UI
     path("dashboard/", dashboard, name="dashboard"),
